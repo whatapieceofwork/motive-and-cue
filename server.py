4d7050b461a97b221866211f369fdfc6d590f840
Alison Bain <amwbain@gmail.com>
2021-05-05 10:55:56 +0000

    Refactored add_film() to use TMBD API; verify_film() now dynamically generates a cast list populated with TMDB actor info and photos (this will eventually be saved, not run each time)

--- server.py
+++ server.py
@@ -3,9 +3,13 @@ import jinja2
 import os
 from bs4 import BeautifulSoup
 import urllib3
+import requests
+import re
+import pprint
+import json
 
 FLASK_KEY = os.environ["FLASK_KEY"]
-MOVIE_API_KEY = os.environ["MOVIE_API_KEY"]
+MOVIEDB_API_KEY = os.environ["MOVIEDB_API_KEY"]
 
 app = Flask(__name__)
 
@@ -36,13 +40,33 @@ def process_film():
     """Processes user input, scrapes IMDB for information."""
 
     play = request.args.get("play")
-    imdb_url = request.args.get("imdb-url")
-    http = urllib3.PoolManager()
-    page = http.request("GET", imdb_url)
-    status = page.status
+    film_url = request.args.get("film-url")
+    moviedb_regx = ("(?<=https:\/\/www\.themoviedb\.org\/movie\/)[0-9]*")
+    moviedb_id = re.search(moviedb_regx, film_url)
+    moviedb_id = moviedb_id[0]
+    moviedb_credits = "https://api.themoviedb.org/3/movie/" + moviedb_id + "/credits?api_key=" + MOVIEDB_API_KEY
+    credits = requests.get(moviedb_credits)
+    credits = credits.json()
+    cast = credits["cast"]
+
+    actors = {}
+    for actor in cast:
+        actor_lname = actor["name"].split()[1].lower()
+        actors[actor_lname] = actor
+        print(actors[actor_lname])
+
+    for actor in actors:
+        id = actors[actor]["id"]
+        actor_profile = requests.get("https://api.themoviedb.org/3/person/" + str(id) + "?api_key=" + str(MOVIEDB_API_KEY))
+        actor_profile = actor_profile.json()
+        actors[actor].update(actor_profile)
+        print(actors[actor])
+
 
     return render_template("verify-film.html",
-                            status = status)
+                            cast = cast,
+                            actors = actors,
+                            )
 
 
 if __name__ == '__main__':
