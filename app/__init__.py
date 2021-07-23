d66f3389e0f3fdc54fe2b207b2bfb23b8d3e57e5
Alison Bain <amwbain@gmail.com>
2021-07-23 11:17:56 +0000

    Expanded base.html, inheriting from bootstrap/base.html. Index displays placeholder timestamp.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, Blueprint
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, login_required, set_login_view
 from flask_login.utils import login_user, current_user, logout_user, login_required
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm.session import object_session
 from sqlalchemy.sql import exists
@@ -26,6 +27,8 @@ def create_app(config_name):
     login_manager = LoginManager(app)
     login_manager.login_view = "login"
     login_manager.init_app(app)
+
+    moment = Moment(app)
 
     @login_manager.user_loader
     def load_user(user_id):
--- app/main/routes.py
+++ app/main/routes.py
@@ -5,6 +5,8 @@ from . import main
 from .. import db
 from ..models import User
 
+        
 @main.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",
+                            current_time=datetime.utcnow())
