ad738c16504c107ecdebae897f24de1d64523893
Alison Bain <amwbain@gmail.com>
2021-08-10 23:50:57 +0000

    Revamped index page to display visual cards using randomly-chosen images from the categories

--- app/main/crud.py
+++ app/main/crud.py
@@ -712,4 +712,31 @@ def delete_object(object):
     db.session.add(object)
     db.session.delete(object)
 
+
+def get_random_image(type):
+
+    if type is Film:
+        type_has_img = db.session.query(exists().where(type.poster_path != None)).scalar()
+        if type_has_img: 
+            options = type.query.filter(type.poster_path != None).all()
+        else:
+            options = random.choice(Film.query.all())
+    elif type is Person:
+        type_has_img = db.session.query(exists().where(type.photo_path != None)).scalar()
+        if type_has_img: 
+            options = type.query.filter(type.photo_path != None).all()
+        else:
+            options = random.choice(Person.query.all())
+    else:
+        type_has_img = db.session.query(exists().where(type.img != None)).scalar()
+        if type_has_img:
+            options = type.query.filter(type.img != None).all()
+        else:
+            options = type.query.all()
+
+    if options:
+        return random.choice(options)
+    else:
+        return None
+
 # ----- END: MISC FUNCTIONS ----- #
